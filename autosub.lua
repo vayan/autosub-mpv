e57c3c2f6f7d8d3e829dac556cbb2c3d847d0f70
Yann Vaillant <yann@vaillant.im>
2016-11-22 09:49:00 +0100

    Better errors handling and some refactoring
    * Fix indentation
    * Assume `subliminal` binary is in the PATH env
    * Refactoring

--- autosub.lua
+++ autosub.lua
@@ -2,23 +2,29 @@
 -- add the following to your input.conf to change the default keybinding:
 -- keyname script_binding auto_load_subs
 local utils = require 'mp.utils'
+
+function display_error()
+  mp.msg.warn("Subtitle download failed: ")
+  mp.osd_message("Subtitle download failed")
+end
+
 function load_sub_fn()
-    subl = "/usr/local/bin/subliminal" -- use 'which subliminal' to find the path
-    mp.msg.info("Searching subtitle")
-    mp.osd_message("Searching subtitle")
-    t = {}
-    t.args = {subl, "download", "-s", "-l", "en", mp.get_property("path")}
-    res = utils.subprocess(t)
-    if res.status == 0 then
-        sub_path = string.gsub(mp.get_property("path"), "%.%w+$", ".srt")
-        mp.osd_message(sub_path)
-        mp.commandv("sub_add", sub_path)
-        mp.msg.info("Subtitle download succeeded")
-        mp.osd_message("Subtitle download succeeded")
+  path = mp.get_property("path")
+  srt_path = string.gsub(path, "%.%w+$", ".srt")
+  t = { args = { "subliminal", "download", "-s", "-l", "en", path } }
+
+  mp.osd_message("Searching subtitle")
+  res = utils.subprocess(t)
+  if res.error == nil then
+    if mp.commandv("sub_add", srt_path) then
+      mp.msg.warn("Subtitle download succeeded")
+      mp.osd_message("Subtitle '" .. srt_path .. "' download succeeded")
     else
-        mp.msg.warn("Subtitle download failed")
-        mp.osd_message("Subtitle download failed")
+      display_error()
     end
+  else
+    display_error()
+  end
 end
 
 mp.add_key_binding("b", "auto_load_subs", load_sub_fn)
